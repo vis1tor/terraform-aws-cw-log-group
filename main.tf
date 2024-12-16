9ffd7292a6121886d053011175673b033760f7a5
mkkim <dlsrks2845@naver.com>
2024-12-16 11:44:12 +0900

    CloudWatch Log Group Subscription Filter added 0.0.1

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ resource "aws_cloudwatch_log_group" "this" {
 
   tags = each.value.cw_log_group_tags
 }
+
+##########################################
+# CloudWatch Log Group Subscription Filter
+##########################################
+resource "aws_cloudwatch_log_subscription_filter" "this" {
+  for_each = { for k, v in var.cw_log_group_info : v.cw_log_group_sub_filter_name => v if v.cw_log_group_sub_filter_name != "" }
+
+  name            = each.value.cw_log_group_sub_filter_name
+  role_arn        = try(each.value.cw_log_group_sub_filter_role_arn, null)
+  log_group_name  = aws_cloudwatch_log_group.this[each.value.cw_log_group_name].name
+  filter_pattern  = each.value.cw_log_group_sub_filter_pattern
+  destination_arn = each.value.cw_log_group_sub_filter_destination_arn
+  distribution    = each.value.cw_log_group_sub_filter_distribution
+}
--- variables.tf
+++ variables.tf
@@ -3,10 +3,15 @@
 ################################################################################
 variable "cw_log_group_info" {
   type = map(object({
-    cw_log_group_name              = string
-    cw_log_group_retention_in_days = string
-    cw_log_group_class             = string
-    cw_log_group_kms_key_arn       = string
-    cw_log_group_tags              = map(string)
+    cw_log_group_name                       = string
+    cw_log_group_retention_in_days          = string
+    cw_log_group_class                      = string
+    cw_log_group_kms_key_arn                = string
+    cw_log_group_tags                       = map(string)
+    cw_log_group_sub_filter_name            = string
+    cw_log_group_sub_filter_role_arn        = string
+    cw_log_group_sub_filter_pattern         = string
+    cw_log_group_sub_filter_destination_arn = string
+    cw_log_group_sub_filter_distribution    = string
   }))
 }
